e1f28e47402b8e80522fbf2da205bb973c5e3c7b
Ya Boy Pax <fleming.paxton@yahoo.com>
2024-10-29 20:28:32 -0700

    update compile script and px_audio

--- CompileToSingleHeader.sh
+++ CompileToSingleHeader.sh
@@ -1,8 +1,22 @@
 
 #!/bin/bash
 
-cat px_globals.h px_memory.h px_vector.h px_buffer.h px_delay.h px_biquad.h px_saturator.h px_clip.h px_equalizer.h px_compressor.h > px_audio.h
+output_file="px_audio.h"
+> "$output_file" 
 
+header_files=("px_globals.h" "px_memory.h" "px_vector.h" "px_buffer.h" "px_delay.h" "px_biquad.h" "px_saturator.h" "px_clip.h" "px_equalizer.h" "px_compressor.h")
+
+cat "${header_files[0]}" >> "$output_file"
+
+for file in "${header_files[@]:1}"; do
+    # Extract lines between the first #ifndef and the corresponding #endif
+    # Exclude lines containing #include
+    sed -n '/#ifndef/,${
+            /#include/d
+            p
+    }' "$file" >> "$output_file"   
+done
+                                                                                                                                               
 git add px_audio.h
 git commit -m "update px_audio.h"
 
